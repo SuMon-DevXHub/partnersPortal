dbaea2fc0ca086145f07866950f470405ec0d6c8
sarafat-ayon <84876431+sarafat-ayon@users.noreply.github.com>
2023-06-13 15:34:12 +0600

    sa-2735-need-to-design-mobile-footer-menu-and-header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Social Media Monitoring Software",
+      titleTemplate: "%s | Ediscovery Service | Sharp Archive",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          name: "keywords",
+          content: process.env.META_KEYWORDS,
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+        {
+          key: "canonical",
+          rel: "canonical",
+          href: process.env.NUXT_PUBLIC_SITE_URL,
+        },
+      ],
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+
+  runtimeConfig: {
+    google: {
+      clientId: "client-id",
+      clientSecret: "client-secret",
+    },
+
+    public: {
+      appName: process.env.APP_NAME,
+      siteUrl: process.env.APP_URL,
+    },
+  },
+  // devtools: { enabled: true },
 
   css: [
     "@fortawesome/fontawesome-svg-core/styles.css",
