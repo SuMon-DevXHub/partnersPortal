254825e3e1a4381019f74e5216b9d67ab4808e76
sarafat-ayon <84876431+sarafat-ayon@users.noreply.github.com>
2023-06-13 17:43:35 +0600

    sa-2723-need-to-design-partners-portal-profile-modal

--- stores/the-footer.ts
+++ stores/the-footer.ts
@@ -1,17 +1,17 @@
-import { defineStore, Store } from 'pinia'
+import { defineStore, Store } from "pinia";
 
 export const useFooterStore = defineStore("footer", () => {
   // state
-  const isShowFooterRef = ref<boolean>(false)
+  const isShowFooterRef = ref<boolean>(false);
 
-  const isShowFooter = computed(() => isShowFooterRef.value)
+  const isShowFooter = computed(() => isShowFooterRef.value);
 
   // actions
-  const setIsShowFooter = (payload: boolean) => {
-    isShowFooterRef.value = !isShowFooterRef.value
-  }
+  const setIsShowFooter = (payload: boolean) => {    
+    isShowFooterRef.value = payload;
+  };
   return {
     isShowFooter,
     setIsShowFooter,
-  }
-})
+  };
+});
